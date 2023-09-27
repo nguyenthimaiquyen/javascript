e919b5d988ff07dfc64753d27c27aee13d604225
maiquyen2403 <132974222+nguyenthimaiquyen@users.noreply.github.com>
2023-09-27 16:17:28 +0700

    day06

---
+++ Day06_Promise_API/lab_dog2/dog_API2.js
@@ -0,0 +1,59 @@
+//lấy ra các element
+const breedList = document.getElementById('breed-list');
+const btn = document.getElementById('btn');
+const image = document.getElementById('image');
+const p1 = document.querySelector('.form p');
+const p2 = document.querySelector('.form p:nth-child(3)');
+
+
+//lấy danh sách các giống dog
+const getBreedList = async () => {
+    try {
+        
+        let res = await fetch('https://dog.ceo/api/breeds/list/all');
+
+        let list = await res.json();
+
+        const arrayList = Object.keys(list.message);
+        console.log(arrayList);
+
+        let html = '';
+        arrayList.forEach((value) => {
+            html += `<option value="">${value}</option>`
+        });
+
+        breedList.innerHTML = html;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getBreedList();
+
+
+//random ảnh 
+breedList.addEventListener('change', () => {            
+    let api = p1.innerText + breedList.options[breedList.selectedIndex].text + p2.innerText;
+    
+    const getImage = async () => {
+        try {
+            let res = await fetch(api);
+            //console.log(res)
+            
+            let data = await res.json();            
+            //console.log(data);
+            
+            image.src = data.message;
+    
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    btn.addEventListener('click', getImage)
+})
+
+//bug: do chọn lắng nghe sự kiện change nên nếu không thay đổi
+// giống dog thì sẽ ko render ra ảnh được
+
+
